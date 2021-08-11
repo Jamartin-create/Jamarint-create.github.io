8a043890abed2538fb251d1e6c67d9fdc724fdb6
Jamartin-create <martin_du@qq.com>
2021-08-11 18:49:31 +0800

    version-2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210811.1549';
+const cacheName = 'chirpy-20210811.1848';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
