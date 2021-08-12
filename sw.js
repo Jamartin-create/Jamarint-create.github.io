86af044ca135b571b8715ac1148b9c16f7143546
Jamartin-create <martin_du@qq.com>
2021-08-12 14:49:43 +0800

    发布3篇文章

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210811.1848';
+const cacheName = 'chirpy-20210812.1447';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
