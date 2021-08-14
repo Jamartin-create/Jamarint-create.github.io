20061bdf051dcdd18e9b4b9297ca5ca76e49fc37
Jamartin-create <martin_du@qq.com>
2021-08-15 02:29:40 +0800

    改名+更博

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210812.1447';
+const cacheName = 'chirpy-20210815.0227';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
