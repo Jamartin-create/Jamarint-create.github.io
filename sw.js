73befcef79199961da06bd2425db04820ed2b328
Jamartin <martin_du@qq.com>
2021-10-04 17:56:22 +0800

    oracle Single table query study

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211003.1611';
+const cacheName = 'chirpy-20211004.1754';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
