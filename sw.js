6e3ceb7f24c7f5b6b981f976bd0ac2340c54ad41
Jamartin <martin_du@qq.com>
2021-10-07 18:08:38 +0800

    add Oracle unSingle Table Query

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211004.1754';
+const cacheName = 'chirpy-20211007.1806';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
