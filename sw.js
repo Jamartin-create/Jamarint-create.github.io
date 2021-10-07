954624a9a6348e28a0b2ac766553a204a8f0bf7f
Jamartin <martin_du@qq.com>
2021-10-07 23:49:42 +0800

    oracle约束学习笔记

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211007.1806';
+const cacheName = 'chirpy-20211007.2348';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
