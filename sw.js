1f60264857a273bc860faaf5cff048bbdb2f51c3
Jamartin <martin_du@qq.com>
2021-11-07 23:36:12 +0800

    commit react basic arragment

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211107.2247';
+const cacheName = 'chirpy-20211107.2334';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
