a79dff82f9dc7a85af959dbf959651ee1706d318
Jamartin <martin_du@qq.com>
2021-11-09 20:14:07 +0800

    format my article

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211109.1942';
+const cacheName = 'chirpy-20211109.2012';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
