0543b384b0e369823c493d438073b6c203b7f8c6
Jamartin <martin_du@qq.com>
2021-11-09 19:36:54 +0800

    format my article and add some photo

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211109.0003';
+const cacheName = 'chirpy-20211109.1935';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
