606dc424293f4beca70a64d384db8136f9c2b7d8
Jamartin <martin_du@qq.com>
2021-12-22 10:25:14 +0800

    vue cli study

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211120.1738';
+const cacheName = 'chirpy-20211222.1024';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
