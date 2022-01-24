67925a2b5d855889e88555589440f4a524441ecd
Jamartin <martin_du@qq.com>
2022-01-25 00:23:31 +0800

    feat: post 全排列算法实现

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211229.1937';
+const cacheName = 'chirpy-20220125.0021';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
