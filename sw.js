b261248486edf36cf9ad8fa858bd95c83c0b7790
Jamartin <martin_du@qq.com>
2022-03-06 22:50:28 +0800

    Servlet Basic

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220125.0027';
+const cacheName = 'chirpy-20220306.2249';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
