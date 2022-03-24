30a029fb68d75d7a382d7da94f9bf94a6b01d6b1
Jamartin <martin_du@qq.com>
2022-03-24 16:52:42 +0800

    feat: 新增Hibernate学习blog

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220306.2249';
+const cacheName = 'chirpy-20220324.1651';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
