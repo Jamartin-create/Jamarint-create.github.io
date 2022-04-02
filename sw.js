cb04361ab8589146f8beaedc10d8623dca51b4c8
Jamartin <martin_du@qq.com>
2022-04-02 20:12:48 +0800

    fix: 头像不能显示的问题

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220401.1956';
+const cacheName = 'chirpy-20220402.2011';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
