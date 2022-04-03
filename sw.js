ea3d6fe9c3fef18543c18234f62b6986fc2813a2
Jamartin <martin_du@qq.com>
2022-04-03 23:17:48 +0800

    postUpdate: Spring 依赖注入、自动装配部分

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220402.2011';
+const cacheName = 'chirpy-20220403.2313';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
